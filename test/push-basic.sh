03f2f757c922590d161cc056b057ec9f7e9a6f08
Mandeep Baines <msb@chromium.org>
2009-11-11 14:28:07 -0800

    Add git push support for case where upstream repo is git.

---
+++ test/push-basic.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+. ./test-lib.sh
+
+setup_initgit
+setup_gitgit
+
+(
+  set -e
+  cd git-git
+  git checkout -q --track -b work origin
+  echo "some work done on a branch" >> test
+  git add test; git commit -q -m "branch work"
+  echo "some other work done on a branch" >> test
+  git add test; git commit -q -m "branch work"
+
+  test_expect_success "git-cl upload wants a server" \
+    "$GIT_CL upload 2>&1 | grep -q 'You must configure'"
+
+  git config rietveld.server localhost:8080
+
+  test_expect_success "git-cl status has no issue" \
+    "$GIT_CL status | grep -q 'no issue'"
+
+  # Prevent the editor from coming up when you upload.
+  export EDITOR=/bin/true
+  test_expect_success "upload succeeds (needs a server running on localhost)" \
+    "$GIT_CL upload -m test master... | grep -q 'Issue created'"
+
+  test_expect_success "git-cl status now knows the issue" \
+    "$GIT_CL status | grep -q 'Issue number'"
+
+  # Push a description to this URL.
+  URL=$($GIT_CL status | sed -ne '/Issue number/s/[^(]*(\(.*\))/\1/p')
+  curl --cookie dev_appserver_login="test@example.com:False" \
+       --data-urlencode subject="test" \
+       --data-urlencode description="foo-quux" \
+       $URL/edit
+
+  test_expect_success "git-cl push ok" \
+    "$GIT_CL push -f"
+
+  git checkout -q master > /dev/null 2>&1
+  git pull -q > /dev/null 2>&1
+
+  test_expect_success "committed code has proper description" \
+      "git show | grep -q 'foo-quux'"
+
+  test_expect_success "issue no longer has a branch" \
+      "git cl status | grep -q 'work: None'"
+
+  test_expect_success "upstream svn has our commit" \
+      "svn log $REPO_URL 2>/dev/null | grep -q 'foo-quux'"
+)
+SUCCESS=$?
+
+cleanup
+
+if [ $SUCCESS == 0 ]; then
+  echo PASS
+fi
--- test/test-lib.sh
+++ test/test-lib.sh
@@ -5,6 +5,7 @@ set -e
 
 PWD=`pwd`
 REPO_URL=file://$PWD/svnrepo
+GITREPO_URL=file://$PWD/gitrepo
 GIT_CL=$PWD/../git-cl
 
 # Set up an SVN repo that has a few commits to trunk.
@@ -34,8 +35,35 @@ setup_gitsvn() {
   git svn -q clone $REPO_URL git-svn >/dev/null 2>&1
 }
 
+# Set up a git repo that has a few commits to master.
+setup_initgit() {
+  echo "Setting up test upstream git repo..."
+  rm -rf gitrepo
+  mkdir gitrepo
+
+  (
+    cd gitrepo
+    git init -q
+    echo "test" > test
+    git add test
+    git commit -qam "initial commit"
+    echo "test2" >> test
+    git commit -qam "second commit"
+    # Hack: make sure master is not the current branch
+    #       otherwise push will give a warning
+    git checkout -q -b foo
+  )
+}
+
+# Set up a git checkout of the repo.
+setup_gitgit() {
+  echo "Setting up test git repo..."
+  rm -rf git-git
+  git clone -q $GITREPO_URL git-git
+}
+
 cleanup() {
-  rm -rf svnrepo svn git-svn
+  rm -rf gitrepo svnrepo svn git-git git-svn
 }
 
 # Usage: test_expect_success "description of test" "test code".
